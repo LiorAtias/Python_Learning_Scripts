f54398b629a50aab84654327598d714a6cff0175
Lior Atias <liorat2011@gmail.com>
2022-09-05 17:26:46 +0300

    another commit 050922

---
+++ highlow.py
@@ -0,0 +1,12 @@
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low, high))
+input("Press ENTER to start")
+
+guess = 1
+while True:
+    guess = low + (high -low) // 2
+    high_low = input("My guess is {}. should I guess higher or lower? "
+    "Enter h or l or c if my guess is correct"
+    .format(guess)).casefoldcasef()
