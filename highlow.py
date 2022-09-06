f287d1e86e0a0c980a349a3e3da93c73e0bc4a90
Lior Atias <liorat2011@gmail.com>
2022-09-06 22:06:45 +0300

    last commit to the guessing game

--- highlow.py
+++ highlow.py
@@ -5,8 +5,9 @@ print("Please think of a number between {} and {}: ".format(low, high))
 input("Press ENTER to start: ")
 
 guesses = 0
-guess = 1
+
 while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. should I guess higher or lower? "
                      "Enter h or l or c if my guess is correct..: "
